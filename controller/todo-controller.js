34a5f92a309aff52dab404fdfa19f9a5e21bacc5
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-04-30 13:19:15 +0600

    First a new todo save in database

--- controller/todo-controller.js
+++ controller/todo-controller.js
@@ -1,7 +1,16 @@
+import Todo from "../model/Todo.js";
 
-
-
-
- export const addNewTodo = (req, res) => {
+export const addNewTodo = async (req, res) => {
+  try {
     console.log(req.body);
-}
+    const newTodo = await Todo.create({
+      data: req.body.newTodo,
+      createdAt: Date.now(),
+    });
+    await newTodo.save();
+
+    return res.status(200).json(newTodo);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
