5270882e1be846a8cc44c74d8241df8f841d4f04
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-05-01 22:56:47 +0600

    all todo part complete'

--- controller/todo-controller.js
+++ controller/todo-controller.js
@@ -14,3 +14,71 @@ export const addNewTodo = async (req, res) => {
     return res.status(500).json(error.message);
   }
 };
+
+export const getAllTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find({}).sort({ createdAt: -1 });
+
+    return res.status(200).json(todos);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export const toggleTodoDone = async (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  try {
+    const previousTodo = await Todo.findById(id);
+    // console.log(todoRef);
+
+    const updatedTodo = await Todo.findOneAndUpdate(
+      { _id: id },
+      { done: !previousTodo.done }
+    );
+
+    previousTodo.done = !previousTodo.done;
+
+    await updatedTodo.save();
+    // console.log(todoRef);
+    return res.status(200).json(previousTodo);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export const updateTodo = async (req, res) => {
+  const id = req.params.id;
+  const todoText = req.body.todoText;
+  console.log(id, todoText);
+  try {
+    const previousTodo = await Todo.findById(id);
+    // console.log(todoRef);
+
+    const updatedTodo = await Todo.findOneAndUpdate(
+      { _id: id },
+      { data: todoText }
+    );
+
+    previousTodo.data = todoText;
+
+    await updatedTodo.save();
+    // console.log(todoRef);
+    return res.status(200).json(previousTodo);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export const deleteTodo = async (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  try {
+    const deletedTodo = await Todo.findOneAndDelete({ _id: id });
+
+    // console.log(todoRef);
+    return res.status(200).json({ message: "successful" });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { addNewTodo } from "../controller/todo-controller.js";
+import {
+    addNewTodo,
+    deleteTodo,
+    getAllTodos,
+    toggleTodoDone,
+    updateTodo,
+} from "../controller/todo-controller.js";
 
 const route = express.Router();
 
-route.post("/addNewTodo", addNewTodo)
-
+route.post("/addNewTodo", addNewTodo);
+route.get("/getAllTodos", getAllTodos);
+route.get("/getAllTodos/:id", toggleTodoDone);
+route.put("/editedTodo/:id", updateTodo);
+route.delete("/deleteTodo/:id", deleteTodo);
 
 export default route;
